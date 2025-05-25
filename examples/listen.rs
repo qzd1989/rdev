25c8fe266fdd112385a8d2866a475610780a8c6e
kiwi <1145567334@qq.com>
2025-05-25 12:24:18 +0800

    1

--- examples/listen.rs
+++ examples/listen.rs
@@ -1,17 +1,19 @@
 use rdev::{Event, listen, set_is_main_thread, stop_listen};
 use std::{thread, time::Duration};
 fn main() {
+    dbg!("hello");
     thread::spawn(|| {
-        #[cfg(target_os = "macos")]
-        set_is_main_thread(false);
+        // only use on tauri
+        // #[cfg(target_os = "macos")]
+        // set_is_main_thread(false);
 
         if let Err(error) = listen(callback) {
             println!("Error: {:?}", error)
         }
     });
-    thread::sleep(Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(1000));
     stop_listen();
-    thread::sleep(Duration::from_secs(10000));
+    thread::sleep(Duration::from_secs(1000));
 }
 
 fn callback(event: Event) {
